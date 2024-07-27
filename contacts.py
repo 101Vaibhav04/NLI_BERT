15139a2fa99b2ea8969f334c5059c9cfa0bb699d
Anjanayae <141940307+Anjanayae@users.noreply.github.com>
2024-07-27 23:09:37 +0530

    Create contacts.py

---
+++ contacts.py
@@ -0,0 +1,9 @@
+import streamlit as st
+st.title("Contact Us")
+st.write("Feel free to contact us")
+
+st.markdown("### Our Team")
+st.write("1. Harsh Anand - mems230005016@iiti.ac.in")
+st.write("2. Kushagra Shrikhande - mems230005024@iiti.ac.in")
+st.write("3. Anjanayae Chaurasia- sse230021002@iiti.ac.in")
+st.write("4. Vaibhav Ojha - mc230041036@iiti.ac.in")
